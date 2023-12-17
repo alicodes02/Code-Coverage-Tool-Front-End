a64323571fb2cbdfde08a113ecf0f951b70efed3
Muhammad Ali <se.muhammadali01@gmail.com>
2023-12-17 19:11:40 +0500

    changed some styles

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import './Button.css';
 
 function App() {
+  
   const [code, setCode] = useState('');
   const [zipFile, setZipFile] = useState(null);
   const [spinner, setSpinner] = useState(false);
@@ -38,16 +39,16 @@ function App() {
   };
 
   const downloadReport = () => {
+
     if (zipFile) {
-      // Create a Blob containing the ZIP file
+      
       const blob = new Blob([zipFile], { type: 'application/zip' });
 
-      // Create a URL for the Blob
       const url = URL.createObjectURL(blob);
 
-      // Create a link element to trigger the download
       const link = document.createElement('a');
       link.href = url;
+
       link.download = 'coverage-report.zip';
       document.body.appendChild(link);
       link.click();
@@ -57,17 +58,18 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Code Coverage Tool</h1>
-      <p>Paste Your Code Here!</p>
-      <textarea required = {true}
-        rows={35}
-        cols={100}
-        value={code}
-        onChange={(e) => setCode(e.target.value)}
-      />
-      <br /> <br />
 
-      <button class="btn" onClick={parseCode}> Submit Code </button>
+      <h1>Code Coverage Tool</h1>
+
+      {zipFile && (
+        <div>
+          <h2>Your Code Coverage Report is Ready!</h2>
+          <p>Download the coverage report:</p>
+          <button class="btn" onClick={downloadReport}>Download Report</button>
+        </div>
+      )}
+
+      <p>Paste Your Code Here!</p>
 
       {
         spinner && (
@@ -76,13 +78,16 @@ function App() {
         )
       }
 
-      {zipFile && (
-        <div>
-          <h2>Code Coverage Report</h2>
-          <p>Download the coverage report:</p>
-          <button class="btn" onClick={downloadReport}>Download Report</button>
-        </div>
-      )}
+      <textarea className='text-box' required = {true}
+        rows={35}
+        cols={100}
+        value={code}
+        onChange={(e) => setCode(e.target.value)}
+      />
+      <br /> <br />
+
+      <button class="btn" onClick={parseCode}> Submit Code </button>
+      
     </div>
   );
 }
