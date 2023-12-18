1fdea946c9c4841297f2d666b35035721ce33e38
Muhammad Ali <se.muhammadali01@gmail.com>
2023-12-18 06:57:29 +0500

    changed backend api url

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     setSpinner(true);
 
     try {
-      const response = await fetch('http://localhost:3001/generate-tests', {
+      const response = await fetch('https://cerulean-newt-fez.cyclic.app/generate-tests', {
         method: 'POST',
         headers: {
           'Content-Type': 'text/plain',
